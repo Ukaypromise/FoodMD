fb3cc25425f9893e89ba045fc771575ce02dc2be
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-30 23:32:38 +0100

    Add comment button

--- src/category.js
+++ src/category.js
@@ -1,33 +1,26 @@
 const fetchFucntion = () => {
   const displayFoods = (foods) => {
-    const foodItemsDiv = document.getElementById('food-items');
+    const foodItemsDiv = document.getElementById("food-items");
 
     foods.meals.forEach((meal) => {
-      const foodDiv = document.createElement('div');
-
-      foodDiv.className = 'meal';
-      const foodInfo = `
-      <p>${meal.strMeal}</p>
+      const foodDiv = document.createElement("div");
+      foodDiv.className = "meal";
+      const foodInfo = `      
       <img src='${meal.strMealThumb}'>
-      <p>${meal.strCategory}</p>
+      <p>${meal.strMeal}</p>
+      <p>Category: ${meal.strCategory}</p>
+      <button class = "comment-btn">Comment</button>
       `;
       foodDiv.innerHTML = foodInfo;
       foodItemsDiv.appendChild(foodDiv);
     });
   };
 
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=e')
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=e")
     .then((res) => res.json())
     .then((data) => displayFoods(data));
 };
 
-// const getMeals = async () => {
-//   const response = await fetch(
-//     'https://www.themealdb.com/api/json/v1/1/filter.php?a=indian',
-//   );
-//   const result = await response.json();
-//   console.log(result)
-//   return result.meals;
-// };
+
 
 export default fetchFucntion;
