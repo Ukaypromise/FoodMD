b28f33c2d70c93b37e3907eb6863dd52e5830e3d
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-31 02:09:48 +0100

    Add functionality to buttons of modal

---
+++ src/modules/displayFoods.js
@@ -0,0 +1,30 @@
+import getMealInfo from "./getMealinfo.js";
+const foodItemsDiv = document.getElementById("food-items");
+
+// event listeners
+foodItemsDiv.addEventListener("click", getMealInfo);
+
+const displayFoods = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=e")
+    .then((res) => res.json())
+    .then((data) => {
+      let html = "";
+      data.meals.forEach((meal) => {
+        html += `
+      <div class="meal-item" data-id = "${meal.idMeal}"> 
+        <div class = "meal-img">     
+          <img src='${meal.strMealThumb}'>
+        </div>
+        <div class = "meal-name">
+          <p>${meal.strMeal}</p>
+          <p>Category: ${meal.strCategory}</p>
+          <button class = "comment-btn">Comment</button>
+        </div>
+      </div>
+      `;
+      });
+      foodItemsDiv.innerHTML = html;
+    });
+};
+
+export default displayFoods;
---
+++ src/modules/mealInfoModal.js
@@ -0,0 +1,26 @@
+
+const modalDetailsContent = document.querySelector(".modal-details-content");
+function mealInfoModal(meal) {
+  console.log(meal);
+  meal = meal[0];
+  let html = `
+        <div class="modal-meal-img">
+              <img src="${meal.strMealThumb}" alt="" />
+            </div>
+            <h2 class="modal-title">${meal.strMeal}</h2>
+            <div class="item-details">
+              <div>
+                <p>${meal.strCategory}</p>
+                <p>${meal.strArea}</p>
+              </div>
+              <div>
+                <p>${meal.strIngredient4}</p>
+                <p>${meal.strMeasure1}</p>
+              </div>
+            </div>
+    `;
+  modalDetailsContent.innerHTML = html;
+  modalDetailsContent.parentElement.classList.add("showModal");
+}
+
+export default mealInfoModal;
