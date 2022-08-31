c0b5b1210e2f749f684997890a3bd1369d62cfea
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-31 12:14:04 +0100

    Fix destructuring linter errors

--- src/modules/mealInfoModal.js
+++ src/modules/mealInfoModal.js
@@ -1,24 +1,31 @@
 const modalDetailsContent = document.querySelector('.modal-details-content');
 function mealInfoModal(meal) {
-  meal = meal[0];
+  const {
+    strMeal,
+    strMealThumb,
+    strCategory,
+    strArea,
+    strIngredient4,
+    strMeasure1,
+  } = meal[0];
+  // meal = meal[0];
   const html = `
         <div class="modal-meal-img">
-              <img src="${meal.strMealThumb}" alt="" />
+              <img src="${strMealThumb}" alt="" />
             </div>
-            <h2 class="modal-title">${meal.strMeal}</h2>
+            <h2 class="modal-title">${strMeal}</h2>
             <div class="item-details">
               <div>
-                <p>${meal.strCategory}</p>
-                <p>${meal.strArea}</p>
+                <p>${strCategory}</p>
+                <p>${strArea}</p>
               </div>
               <div>
-                <p>${meal.strIngredient4}</p>
-                <p>${meal.strMeasure1}</p>
+                <p>${strIngredient4}</p>
+                <p>${strMeasure1}</p>
               </div>
             </div>
     `;
   modalDetailsContent.innerHTML = html;
   modalDetailsContent.parentElement.classList.add('showModal');
 }
-
 export default mealInfoModal;
