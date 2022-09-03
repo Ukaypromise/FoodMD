a182abc1ee206c9d0f7814ed2f4d36bdff43b057
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-09-03 16:25:37 +0100

    Fix modal close on click outside modal

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './style.css';
 import displayFoods from './modules/displayFoods.js';
 
 displayFoods();
+
+// close modal window when clicking outside of it
+const modal = document.querySelector(".meal");
+
+
+const modalDetailsContent = document.querySelector('.modal-details-content');
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modalDetailsContent.parentElement.classList.remove("showModal");
+  }
+});
+
+
